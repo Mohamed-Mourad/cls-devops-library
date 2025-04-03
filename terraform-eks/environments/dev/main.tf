3849d544b3c61692902089257de453f9d27c9641
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2025-04-03 21:24:46 +0200

    ingress 10250 worker node sg

--- terraform-eks/environments/dev/main.tf
+++ terraform-eks/environments/dev/main.tf
@@ -19,6 +19,13 @@ resource "aws_security_group" "worker_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  from_port   = 10250
+  to_port     = 10250
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   # Allow all outbound traffic (EFS, EKS API, etc.)
   egress {
     from_port   = 0
