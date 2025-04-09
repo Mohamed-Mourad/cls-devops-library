e4811ebea1040ec4842acfdf1432d253e791ff60
Mohamed Mourad <mohamed.m_2000@outlook.com>
2025-04-09 12:12:54 +0200

    new IaC

--- terraform-eks/modules/load-balancer/main.tf
+++
@@ -1,7 +0,0 @@
-# AWS Load Balancer Controller setup using IRSA
-module "eks_aws_load_balancer_controller" {
-  source  = "akw-devsecops/eks/aws//modules/aws-load-balancer-controller"
-  version = "4.1.0"
-
-  oidc_provider_arn = var.oidc_provider_arn
-}
--- terraform-eks/modules/node-group/outputs.tf
+++
@@ -1,6 +0,0 @@
-// Outputs from the Node Group module
-
-output "node_group_id" {
-  description = "The ID of the EKS node group"
-  value       = aws_eks_node_group.this.id
-}
